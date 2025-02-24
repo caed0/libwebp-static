1a8a263f49834c246b40dc7e6d51437058aff4e3
caed0 <62652990+caed0@users.noreply.github.com>
2025-02-24 14:58:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const fs = require("fs");
+
+const platform = process.platform;
+const arch = process.arch;
+const binaryPath = path.join(__dirname, "binaries", platform, arch, "bin");
+const binaries = {};
+
+if (fs.existsSync(binaryPath)) {
+    const files = fs.readdirSync(binaryPath);
+    files.forEach(file => { binaries[file] = path.join(binaryPath, file); });
+} else {
+    console.error(`Prebuilt binary for ${platform}-${arch} not found!`);
+    process.exit(1);
+}
+
+module.exports = binaries;
+
+
+
+
+
+
---
+++ install.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const fs = require("fs");
+const { execSync } = require("child_process");
+
+
+
+const LIBWEBP_VERSION = "1.5.0";
+const BASE_URL = `https://storage.googleapis.com/downloads.webmproject.org/releases/webp/`;
+
+const PRECOMPILED_BINARIES = {
+    "win32-x64": "windows-x64.zip",
+    "linux-x64": "linux-x86-64.tar.gz",
+    "linux-arm64": "linux-aarch64.tar.gz",
+    "darwin-x64": "mac-x86-64.tar.gz",
+    "darwin-arm64": "mac-arm64.tar.gz"
+}
+
+if (!fs.existsSync(path.join(__dirname, "binaries"))) fs.mkdirSync(path.join(__dirname, "binaries"));
+
+for (const [key, value] of Object.entries(PRECOMPILED_BINARIES)) {
+    const [platform, arch] = key.split("-");
+    const file = `libwebp-${LIBWEBP_VERSION}-${value}`;
+    const binaryUrl = `${BASE_URL}${file}`;
+
+    try {
+        execSync(`curl -s -L ${binaryUrl} --output binaries/${file}`, { stdio: "inherit" });
+
+        if (!fs.existsSync(`binaries/${platform}`)) fs.mkdirSync(`binaries/${platform}`, { recursive: true });
+
+        if (file.endsWith(".zip")) execSync(`unzip -qo binaries/${file} -d binaries/${platform}`, { stdio: "inherit" });
+        else execSync(`tar -xzf binaries/${file} -C binaries/${platform}`, { stdio: "inherit" });
+        if (fs.existsSync(`binaries/${file}`)) fs.unlinkSync(`binaries/${file}`);
+
+        const extractedDir = file.split(".").slice(0, 3).join(".");
+        if (fs.existsSync(`binaries/${platform}/${extractedDir}`))
+            fs.renameSync(`binaries/${platform}/${extractedDir}`, `binaries/${platform}/${arch}`);
+    } catch (error) {
+        if (fs.existsSync("binaries")) fs.rmdirSync("binaries", { recursive: true });
+        console.error("Failed to download prebuilt binary:", error);
+    }
+}
